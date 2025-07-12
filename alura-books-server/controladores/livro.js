358425a7e34f57fffd429c83022fede046a04a54
jvtrfortunato <joaovitor.fortunato@hotmail.com>
2025-07-12 01:48:47 -0300

    💾 Alura Books Server - usando JSON para armazenar dados

--- alura-books-server/controladores/livro.js
+++ alura-books-server/controladores/livro.js
@@ -1,6 +1,9 @@
+const { getTodosLivros } = require("../servicos/livro")
+
 function getLivros(req, res) {
     try {
-        res.send("Olá mundo da Alura do Brasil!")
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
